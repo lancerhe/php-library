108bdac1ab59877bc0fe079a3d117ffa31aa0bb0
Lancer He <lancer.he@gmail.com>
2015-11-01 14:29:05 +0800

    增加按照ID指定位数倍数进行分片的方法

--- src/Library/Algorithm/Partition.php
+++ src/Library/Algorithm/Partition.php
@@ -13,7 +13,23 @@ namespace Library\Algorithm;
 class Partition{
 
     /**
-     * 根据Id分片
+     * 根据位数进行分片，适用于整形，如按照千位数分片 [10000, 19999] => 10
+     * @access public
+     * @static
+     * 
+     * @param  int     $id     id
+     * @param  int     $figure default 1000  按照千位数分片
+     * @return mixed          分片标识
+     */
+    public static function getIndexByFigure($id, $figure = 1000) {
+        if ( 0 != $figure % 10 || $figure <= 0) 
+            trigger_error("Arithmetic_Partition::getIndexByFigure figure needs to be divisible by 10.", E_USER_ERROR);
+
+        return intval($id / $figure);
+    }
+
+    /**
+     * 根据Id分片，适用于整形/哈希值
      * @access public
      * @static
      * 
@@ -21,9 +37,9 @@ class Partition{
      * @param  int     $num   分片数量10或者36,默认10
      * @return mixed          分片标识
      */
-    public static function getIndexById($id, $num=10){
+    public static function getIndexById($id, $num = 10){
         if ( ! in_array( $num, array(10, 36) ) ) 
-            trigger_error("Arithmetic_Partition::getPartitionById partition num only 10/36", E_USER_ERROR);
+            trigger_error("Arithmetic_Partition::getPartitionById partition num only 10/36.", E_USER_ERROR);
 
         switch($num){
             case 10: 
--- tests/Library/Algorithm/PartitionTest.php
+++ tests/Library/Algorithm/PartitionTest.php
@@ -14,7 +14,7 @@ class PartitionTest extends \PHPUnit_Framework_TestCase {
     /**
      * @test
      */
-    public function getPartitionIndexWithError() {
+    public function trigger_error_while_type_in_error_num_15_on_method_getIndexById() {
         $this->setExpectedException('PHPUnit_Framework_Error');
         Partition::getIndexById(4, 15);
     }
@@ -22,16 +22,52 @@ class PartitionTest extends \PHPUnit_Framework_TestCase {
     /**
      * @test
      */
-    public function getPartitionIndexWith10Num() {
-        $index = Partition::getIndexById(4, 10);
-        $this->assertEquals('8', $index);
+    public function id_4_should_be_return_partition_8_on_method_getIndexById_while_num_is_8() {
+        $this->assertEquals('8', Partition::getIndexById(4, 10));
     }
 
     /**
      * @test
      */
-    public function getPartitionIndexWith36Num() {
-        $index = Partition::getIndexById(4, 36);
-        $this->assertEquals('S', $index);
+    public function id_4_should_be_return_partition_S_on_method_getIndexById_while_num_is_36() {
+        $this->assertEquals('S', Partition::getIndexById(4, 36));
+    }
+
+    /**
+     * @desc provider for trigger_error_while_type_in_error_figure_on_method_getIndexByFigure.
+     */
+    public function error_figure() {
+        return [
+            ["1001"], ["0"], ["-2"]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider error_figure
+     */
+    public function trigger_error_while_type_in_error_figure_on_method_getIndexByFigure($figure) {
+        $this->setExpectedException('PHPUnit_Framework_Error');
+        Partition::getIndexByFigure(4, $figure);
+    }
+
+    /**
+     * @desc provider for assert_from_provider_getIndexByFigures.
+     */
+    public function assert_figure() {
+        return [
+            ["0",   "234",    "1000"],
+            ["0",   "999",    "1000"],
+            ["1",   "1000",   "1000"],
+            ["101", "101000", "1000"],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider assert_figure
+     */
+    public function assert_from_provider_getIndexByFigure($expected, $id, $figure) {
+        $this->assertEquals($expected, Partition::getIndexByFigure($id, $figure));
     }
 }
