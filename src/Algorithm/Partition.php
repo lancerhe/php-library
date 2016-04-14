7278742986aafdc6754e794dce3864e23764bb33
Lancer He <lancer.he@gmail.com>
2016-04-14 15:30:33 +0800

    Refactor.

--- src/Algorithm/Partition.php
+++ src/Algorithm/Partition.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * 数据分表算法 
+ * 数据分表算法
  *
  * @category Library
  * @package  Algorithm
  * @author   Lancer He <lancer.he@gmail.com>
- * @version  1.0 
+ * @version  1.0
  */
 
 namespace Library\Algorithm;
